459d346d1fbe4196302e51e80ffd8a5ae46e8991
Milind Bhor <94182634+bhormilind-dev@users.noreply.github.com>
2022-02-28 21:02:33 -0500

    Imp Commands

---
+++ code/FireBaseCode/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'firestorecourse-1b89f',
+    appId: '1:245674331842:web:16266e802098fd8a2db55b',
+    storageBucket: 'firestorecourse-1b89f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBOyVKKaLvlULY1oc6QjbMkoxp-FOA-ZQI',
+    authDomain: 'firestorecourse-1b89f.firebaseapp.com',
+    messagingSenderId: '245674331842',
+  },
+  production: true
+};
